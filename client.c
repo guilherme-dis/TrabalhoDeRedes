222f00443f05bbc42928e0937e2f378d64ee4042
danieldiasbarbo <daniel.dias@ufu.br>
2023-02-01 12:32:27 -0300

    include new thread

--- client.c
+++ client.c
@@ -14,94 +14,139 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
-#define PORT 3490 // the port client will be connecting to 
+#define PORT 3490 // the port client will be connecting to
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 100 // max number of bytes we can get at once
 
-enum t_state {
+struct paramRotina
+{
+    int sockfd;
+    int kill;
+};
+
+enum t_state
+{
     IDLE,
     CONNECTED,
     HHHH
 } state;
 
-int sockfd, numbytes;  
+int sockfd, numbytes;
 
 void sigControlC()
 {
-     printf("Control C pressed\n");
-     close(sockfd);
-     exit(0);
+    printf("Control C pressed\n");
+    close(sockfd);
+    exit(0);
 }
 
 void sigHandler()
 {
-      fprintf(stderr, "*** Pegou um sinal ***\n");
+    fprintf(stderr, "*** Pegou um sinal ***\n");
+}
+
+int *rotina_mensagem(struct paramRotina *param)
+{
+    char str[100] = "loggin daniel.dias@ufu.br senha";
+
+    while (1)
+    {
+        sleep(5);
+        if (param->kill == 0)
+        {
+            printf("rotina rodando\n");
+            write(param->sockfd, str, strlen(str));
+        }
+        else
+        {
+            pthread_exit(0);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	char iBuf[MAXDATASIZE+1];
-	char oBuf[MAXDATASIZE+1];
-	struct hostent *he;
-	struct sockaddr_in their_addr; // connector's address information 
-        int myPort;
 
-        signal(1, sigHandler);
-        signal(SIGPIPE, SIG_IGN);
-        signal(SIGINT, sigControlC);
+    char iBuf[MAXDATASIZE + 1];
+    char oBuf[MAXDATASIZE + 1];
+    struct hostent *he;
+    struct sockaddr_in their_addr; // connector's address information
+    int myPort;
 
-state = IDLE;
+    signal(1, sigHandler);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, sigControlC);
 
-	if (argc != 3) {
-	    fprintf(stderr,"Usage: %s <Hostname> <Port Number>\n", argv[0]);
-	    exit(1);
-	}
+    state = IDLE;
 
-	if ((he=gethostbyname(argv[1])) == NULL) {  // get the host info 
-	    perror("gethostbyname");
-	    exit(1);
-	}
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <Hostname> <Port Number>\n", argv[0]);
+        exit(1);
+    }
 
-        myPort = atoi(argv[2]);
+    if ((he = gethostbyname(argv[1])) == NULL)
+    { // get the host info
+        perror("gethostbyname");
+        exit(1);
+    }
 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-	    perror("socket");
-	    exit(1);
-	}
+    myPort = atoi(argv[2]);
 
-	their_addr.sin_family = AF_INET;    // host byte order 
-	their_addr.sin_port = htons(myPort);  // short, network byte order 
-	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-	memset(&(their_addr.sin_zero), '\0', 8);  // zero the rest of the struct 
-  
-        printf("Going to be connected ....\n");
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
 
-	if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1) {
-	    perror("connect");
-            fprintf(stderr, "connect: ERRNO %d\n", errno);
-	    exit(1);
-	}
+    their_addr.sin_family = AF_INET;     // host byte order
+    their_addr.sin_port = htons(myPort); // short, network byte order
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    memset(&(their_addr.sin_zero), '\0', 8); // zero the rest of the struct
 
-        printf("Connected ....\n");
-        while (1) {
-           memset(oBuf, 0, MAXDATASIZE);
-           printf("Input: ");
-           fgets(oBuf, MAXDATASIZE-1, stdin);
-           oBuf[strlen(oBuf)-1] = 0;
+    printf("Going to be connected ....\n");
 
-           write(sockfd, oBuf, strlen(oBuf));
+    if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1)
+    {
+        perror("connect");
+        fprintf(stderr, "connect: ERRNO %d\n", errno);
+        exit(1);
+    }
 
-           printf("Waiting....\n");
-           memset(iBuf, 0, MAXDATASIZE);
-	   if ((numbytes=read(sockfd, iBuf, MAXDATASIZE)) > 0) {
-               printf("Received [%s] with %d bytes\n", iBuf, numbytes);
-           }
-           if (!strcmp(iBuf, "quit")) {
-               printf("Connection closed\n");
-               close(sockfd);
-               exit(0);
-           }
+    printf("Connected ....\n");
+
+    pthread_t rotina;
+    struct paramRotina param;
+    param.kill = 0;
+    param.sockfd = sockfd;
+    int final;
+
+    pthread_create(&rotina, NULL, rotina_mensagem, &param);
+
+    while (1)
+    {
+        memset(oBuf, 0, MAXDATASIZE);
+        printf("Input: ");
+        fgets(oBuf, MAXDATASIZE - 1, stdin);
+        oBuf[strlen(oBuf) - 1] = 0;
+
+        write(sockfd, oBuf, strlen(oBuf));
+
+        printf("Waiting....\n");
+        memset(iBuf, 0, MAXDATASIZE);
+        if ((numbytes = read(sockfd, iBuf, MAXDATASIZE)) > 0)
+        {
+            printf("Received [%s] with %d bytes\n", iBuf, numbytes);
         }
+        if (!strcmp(iBuf, "quit"))
+        {
+            printf("Connection closed\n");
+            param.kill = 0;
+            pthread_join(rotina, &final);
+            close(sockfd);
+            exit(0);
+        }
+    }
 }
-
